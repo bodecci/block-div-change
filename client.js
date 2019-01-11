58a03fe971a56ae62a5941e4cdab45479934676e
Bode Falade <bode.falade@outlook.com>
2019-01-11 14:47:49 -0600

    add new div to DOM with JQuery.

---
+++ client.js
@@ -0,0 +1,20 @@
+let counter = 0;
+
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('JQ loaded');
+    
+    $('#generateBttn').on('click', function (){
+        counter++;
+        let displayCounter = $('.container');
+        let outputString = `<div class="ketchup">`;
+        outputString += `<p id="pTag">Counter is: ${counter}</p>`;
+        outputString += `<button type="button" id="swapBtn"
+                        class="btn btn-success">Swap</button>
+                        <button type="button" id="deleteBtn"
+                        class="btn btn-danger">Delete</button>`;
+        outputString += `</div><br>`;
+        displayCounter.append(outputString);
+    });
+}
